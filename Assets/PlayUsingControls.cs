e103df939ae0b4e1ac7a9cc74bd72ee8f1941ff1
Vipin Verma <vipin8169@gmail.com>
2020-09-29 14:21:57 -0700

    Added a script to control the audio play using "P" instead of playing on awake. This is done for better recording, allowing time to start the recorder.

---
+++ Assets/PlayUsingControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayUsingControls : MonoBehaviour
+{
+    public AudioSource source;
+    private bool hasPlayed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P) && !hasPlayed)
+        {
+            source.Play();
+            hasPlayed = true;
+        }
+    }
+}
